bcc9602fc1d2c3352ee95a0d9fbdfad15606ee51
Janani Sree <153414043+jananisree96@users.noreply.github.com>
2024-06-23 00:48:43 +0530

    BannerSection

--- latest/restraurant-app/src/App.jsx
+++ latest/restraurant-app/src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import './index.css';
+import Navbar from './components/Navbar';
+import Banner from './components/Banner/Banner';
 import Hero from './components/Hero/Hero'
 
 const App = () => {
   return (
     <div className='overflow-x-hidden'>
+      <Navbar />
       <Hero />
+      <Banner />
     </div>
   )
 }
--- latest/restraurant-app/src/components/Hero/Hero.jsx
+++ latest/restraurant-app/src/components/Hero/Hero.jsx
@@ -40,4 +40,4 @@ const Hero = () => {
   )
 }
 
-export default Hero
+export default Hero;
