524f0cb15f1c0acf268591ed606548e755d4fd58
Janani Sree <153414043+jananisree96@users.noreply.github.com>
2024-06-22 20:06:18 +0530

    Herosection

--- latest/restraurant-app/src/App.jsx
+++ latest/restraurant-app/src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import Navbar from './components/Navbar'
+import './index.css';
+import Hero from './components/Hero/Hero'
 
 const App = () => {
   return (
     <div className='overflow-x-hidden'>
-      <Navbar />
+      <Hero />
     </div>
   )
 }
