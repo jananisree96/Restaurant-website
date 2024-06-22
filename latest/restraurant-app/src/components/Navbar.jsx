bd044a4c86afbefed41eb5992de2fc5b70704cb7
Stephen <johnstephen2662@gmail.com>
2024-06-22 19:56:48 +0530

    NavBar Created

--- latest/restraurant-app/src/components/Navbar.jsx
+++ latest/restraurant-app/src/components/Navbar.jsx
@@ -1,9 +1,80 @@
-import React from 'react'
+import React from 'react';
+import { FaCaretDown, FaUser } from "react-icons/fa";
+
+const NavLinks = [
+  {
+    id: 1,
+    name: "Home",
+    link: "/#"
+  },
+  {
+    id: 2,
+    name: "About",
+    link: "/#"
+  },
+  {
+    id: 3,
+    name: "Contact",
+    link: "/#"
+  }
+];
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
+    <>
+      <div className='bg-white shadow-md'>
+        <div className="container flex justify-between py-4 sm:py-3">
+          {/* ----------Navbar-------- */}
+          <div className='font-bold text-3xl'>Logo</div>
+          <div>
+            <ul className='sm:flex items-center gap-10'>
+              {NavLinks.map(({ id, name, link }) => (
+                <li key={id}>
+                  <a href={link} className='inline-block hover:text-primary text-xl font-semibold'>{name}</a>
+                </li>
+              ))}
+
+              <li className='cursor-pointer group'>
+                <a href="/#" className='inline-block hover:text-primary text-xl font-semibold'>
+                  <div className='flex items-center gap-[2px] py-2'>
+                    Dropdown
+                    <FaCaretDown className='group-hover:rotate-180 duration-300 ml-1' />
+                  </div>
+                </a>
+
+                <div className='absolute z-[9999] hidden group-hover:block w-[200px] bg-white text-black shadow-md p-2'>
+                  <ul>
+                    <li>
+                      <a href="/#" className='text-xl inline-block w-full rounded-md p-2 hover:bg-primary/20'>
+                        Link 1
+                      </a>
+                    </li>
+                    <li>
+                      <a href="/#" className='text-xl inline-block w-full rounded-md p-2 hover:bg-primary/20'>
+                        Link 2
+                      </a>
+                    </li>
+                    <li>
+                      <a href="/#" className='text-xl inline-block w-full rounded-md p-2 hover:bg-primary/20'>
+                        Link 3
+                      </a>
+                    </li>
+                    
+                  </ul>
+                </div>
+              </li>
+              <li>
+                <button className='flex justify-center items-center gap-2 bg-secondary text-xl h-[40px] text-white px-5 py-2 hover:scale-105 duration-300'>
+                  <FaUser />
+                   My Account
+                </button>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Navbar
+export default Navbar;
