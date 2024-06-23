8b4cd8e2ba759702ed4e0045d36a2140297af644
Stephen <johnstephen2662@gmail.com>
2024-06-23 07:20:13 +0530

    Your commit message

--- latest/restraurant-app/src/App.jsx
+++ latest/restraurant-app/src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import './index.css';
 import Hero from './components/Hero/Hero'
+import Navbar from './components/Navbar/Navbar';
 
 const App = () => {
   return (
     <div className='overflow-x-hidden'>
+      <Navbar/>
       <Hero />
+      
     </div>
   )
 }
--- latest/restraurant-app/tailwind.config.js
+++ latest/restraurant-app/tailwind.config.js
@@ -5,11 +5,7 @@ export default {
     extend: {
       colors: {
         primary: "#E83667",
-<<<<<<< HEAD
-        primaryDark: "#BB2C51",
-=======
         primaryDark: "#BB3C51",
->>>>>>> bd044a4c86afbefed41eb5992de2fc5b70704cb7
         secondary: "#21D4B9",
         dark: "#303030",
       },
@@ -22,13 +18,12 @@ export default {
         padding: {
           DEFAULT: "1rem",
           sm: "2rem",
-<<<<<<< HEAD
+
           md: "3rem",
           lg: "4rem",
-=======
           lg: "3rem",
           xl: "4rem",
->>>>>>> bd044a4c86afbefed41eb5992de2fc5b70704cb7
+
         },
       },
     },
